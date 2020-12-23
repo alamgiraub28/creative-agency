fc4ec17d39fc3fc6c23ae5ca28f1159b2689c069
Alamgir Hossain <alamgiraub28@gmail.com>
2020-12-24 01:09:47 +0600

    updated app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import Home from './components/Home/Home';
+import Header from './components/Home/Header/Header';
 
 function App() {
   return (
     <div>
    <Home></Home>
-   
+   <Header></Header>
     </div>
   );
 }
